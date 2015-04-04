40a0feae945718cacf045c7b1ed9dfb02c5e5b9a
Kriti Bajoria <kriti.bajoria@gmail.com>
2015-04-04 16:28:42 +0530

    First Commit

---
+++ strC.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+string reverse(string S) {]
+    int l =S.size() - 1;
+	for(int i = 0;i<l;i++) {
+	   swap(S[i],S[l--]);
+    }
+	
+	return S;
+}
+
+int main() {
+	string s;
+	cin >> s;
+	
+	cout << reverse(s);
+	
+	return 0;
+}
+	
